e7ec4435fe65861bc4db5bf9bd24df34820c9d90
Alexander Seton <alexanderseton@protonmail.com>
2021-11-28 17:44:44 +0000

    Dark mode added.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import EmailContainer from './containers/EmailContainer';
 
 function App() {
+
+  const handleDarkModeClick = function() {
+    const documentBody = document.body;
+    documentBody.classList.toggle("dark-mode");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Temporary Email</h1>
+        <button onClick={handleDarkModeClick} className="dark-mode-button">Dark Mode</button>
       </header>
       <main className="Body">
         <EmailContainer />
--- src/components/EmailHeader.js
+++ src/components/EmailHeader.js
@@ -2,11 +2,17 @@ import React from "react";
 import "./static/EmailHeader.css";
 
 const EmailHeader = ({emailAddress}) => {
+
+    const handleCopyClick = function() {
+        navigator.clipboard.writeText(emailAddress);
+    }
+
     return(
         <div>
             <div className="email-address-display">
                 <h3>Your Temporary Email: </h3>
                 <p>{emailAddress}</p>
+                <button onClick={handleCopyClick} id="copy-button">Copy</button>
             </div>
         </div>
     )
